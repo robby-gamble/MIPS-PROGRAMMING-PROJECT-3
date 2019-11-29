81c52e3ab1ef64d61e8ace4dffa9e35594683f0b
Robby Gamble <robby.gamble2017@gmail.com>
2019-11-28 20:10:14 -0500

    added the file and created the data section and allowed input.

---
+++ 3.s
@@ -0,0 +1,16 @@
+.data
+
+data: .space 1001
+output: .asciiz"\n"
+notvalid: asciiz "NaN"
+comma: .asciiz ","
+
+.text
+main:
+
+li $v0, 8
+la $a0, data #Getting User Input
+li $a1. 1001
+syscall
+
+
